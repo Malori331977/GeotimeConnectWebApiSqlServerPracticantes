996877052d45123f1be8cf4f2952baecfa7d3409
Marlon Loria <marlon.loria@gsitcr.com>
2023-12-15 18:43:24 -0600

    mas cambios a empleados

---
+++ GeotimeConnectWebApi/Controllers/EmpleadoTotalController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GeoTimeConnectWebApi.Data;
+using GeoTimeConnectWebApi.Data.Interfaz;
+using GeoTimeConnectWebApi.Models;
+using System.Text.Json;
+using GeoTimeConnectWebApi.Models.Response;
+
+namespace GeoTimeConnectWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class EmpleadoTotalController : ControllerBase
+    {
+        private readonly IGeoTimeConnectService _repoGT;
+
+        public EmpleadoTotalController(IGeoTimeConnectService repoGT)
+        {
+            _repoGT = repoGT;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<cEmpleado>> Get() => await _repoGT.GetEmpleadoTotal();
+
+        
+
+
+    }
+}
