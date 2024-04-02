5f4b2650d4d79e416d8d1c34fb8888c0eb32fa3b
Allan19Prieto <allanbanilla@gmail.com>
2024-04-02 14:46:59 -0600

    PhPlanillaByUsuario

---
+++ GeotimeConnectWebApi/Controllers/PhPlanillaByUsuarioController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using GeoTimeConnectWebApi.Data;
+using GeoTimeConnectWebApi.Data.Interfaz;
+using GeoTimeConnectWebApi.Models;
+using GeoTimeConnectWebApi.Models.Utils;
+using System.Text.Json;
+using GeotimeConnectWebApi.Models;
+using GeoTimeConnectWebApi.Models.Response;
+
+namespace GeoTimeConnectWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class PhPlanillaByUsuarioController : Controller
+    {
+        private readonly IGeoTimeConnectService _repoGT;
+        public PhPlanillaByUsuarioController(IGeoTimeConnectService repoGT)
+        {
+            _repoGT = repoGT;
+        }    
+
+        [HttpGet("{idUsuario}")]
+        public async Task<List<cPh_Planilla>> Get(string idUsuario) => await _repoGT.GetPhPlanillaByUsuario(idUsuario);
+
+       
+
+    }
+}
