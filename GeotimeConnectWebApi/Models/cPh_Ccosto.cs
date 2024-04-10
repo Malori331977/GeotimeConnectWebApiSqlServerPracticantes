41c36f666dfa830e9bba260da4fb297ade74b437
Allan19Prieto <allanbanilla@gmail.com>
2024-04-09 23:56:51 -0600

    Ccostos

---
+++ GeotimeConnectWebApi/Models/cPh_Ccosto.cs
@@ -0,0 +1,9 @@
+﻿namespace GeoTimeConnectWebApi.Models
+{
+    public class cPh_Ccosto
+    {
+        public string IDCCOSTO { get; set; }
+        public string? DESCRIPCION { get; set; }
+        public char? DISTRIBUYE { get; set; }
+    }
+}
