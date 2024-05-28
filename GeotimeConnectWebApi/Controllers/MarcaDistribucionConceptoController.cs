c9ea89f328e27ef091d17a4c0c0b953620cec736
Allan19Prieto <allanbanilla@gmail.com>
2024-05-27 20:00:11 -0600

    Marcas Dirtibucion Concepto

---
+++ GeotimeConnectWebApi/Controllers/MarcaDistribucionConceptoController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using GeoTimeConnectWebApi.Data;
+using GeoTimeConnectWebApi.Data.Interfaz;
+using GeoTimeConnectWebApi.Models;
+using GeoTimeConnectWebApi.Models.Utils;
+using System.Text.Json;
+using GeoTimeConnectWebApi.Models.Request;
+using GeoTimeConnectWebApi.Models.Response;
+
+namespace GeoTimeConnectWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class MarcaDistribucionConceptoController : Controller
+    {
+        private readonly IGeoTimeConnectService _repoGT;
+        public MarcaDistribucionConceptoController(IGeoTimeConnectService repoGT)
+        {
+
+            _repoGT = repoGT;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<cMarcaDistribucionConcepto>> Get() => await _repoGT.GetMarcaDtnConcepto();      
+
+        [HttpGet("{idregistro}")]
+        public async Task<cMarcaDistribucionConcepto> Get(int idregistro) => await _repoGT.GetMarcaDtnConcepto(idregistro);
+
+        [HttpGet("{idnumero}/{fecha}/{idplanilla}")]
+        public async Task<cMarcaDistribucionConcepto> Get(string idnumero, string fecha, string idplanilla) => await _repoGT.GetMarcaDtnConcepto(idnumero, fecha, idplanilla);
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] IEnumerable<cMarcaDistribucionConcepto> marcaDC)
+        {
+            EventResponse respuesta = await _repoGT.Sincronizar_MarcaDtnConcepto(marcaDC);
+
+            if (respuesta.Id != "0")
+                return BadRequest(respuesta);
+
+            return Ok(respuesta);
+        }
+
+    }
+}
