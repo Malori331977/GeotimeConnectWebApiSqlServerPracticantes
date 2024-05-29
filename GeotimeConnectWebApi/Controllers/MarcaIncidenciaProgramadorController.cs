a3d9e4f001f021f3a0951c66a6f459ec97529b26
Allan19Prieto <allanbanilla@gmail.com>
2024-05-28 23:13:02 -0600

    Marca Incidencia Programador

---
+++ GeotimeConnectWebApi/Controllers/MarcaIncidenciaProgramadorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using GeoTimeConnectWebApi.Data;
+using GeoTimeConnectWebApi.Data.Interfaz;
+using GeoTimeConnectWebApi.Models;
+using GeoTimeConnectWebApi.Models.Utils;
+using System.Text.Json;
+using GeoTimeConnectWebApi.Models.Request;
+using GeoTimeConnectWebApi.Models.Response;
+
+namespace GeoTimeConnectWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class MarcaIncidenciaProgramadorController : Controller
+    {
+        private readonly IGeoTimeConnectService _repoGT;
+        public MarcaIncidenciaProgramadorController(IGeoTimeConnectService repoGT)
+        {
+            _repoGT = repoGT;
+        }
+
+        [HttpGet("{idnumero}/{fecha}/{idplanilla}")]
+        public async Task<IEnumerable<cMarcaIncidencia>> Get(string idnumero, string fecha, string idplanilla) => await _repoGT.GetMarcaIncidenciaProgramador(idnumero,fecha,idplanilla);
+
+    }
+}
