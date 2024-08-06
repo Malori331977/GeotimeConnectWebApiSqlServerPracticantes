a5b88a1911dd3c79ada535d1d4fd9d263c9aea17
Allan19Prieto <allanbanilla@gmail.com>
2024-08-06 15:28:35 -0600

    Ajuste eliminar Accion Personal

--- GeotimeConnectWebApi/Controllers/AccionPersonalCAController.cs
+++ GeotimeConnectWebApi/Controllers/AccionPersonalCAController.cs
@@ -35,5 +35,16 @@ namespace GeoTimeConnectWebApi.Controllers
             return Ok(respuesta);
         }
 
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Int64 id)
+        {
+            EventResponse respuesta = await _repoGT.Elimina_AccionPersonal(id);
+
+            if (respuesta.Id != "0")
+                return BadRequest(respuesta);
+
+            return Ok(respuesta);
+        }
+
     }
 }
