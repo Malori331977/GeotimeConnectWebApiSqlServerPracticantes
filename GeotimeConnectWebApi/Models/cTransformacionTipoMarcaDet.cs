67d9dfcf18cb68adc07132b66e06cc86d3064675
Allan19Prieto <allanbanilla@gmail.com>
2025-02-28 07:33:51 -0600

    Ajuste en modelo TransformacionTipo

--- GeotimeConnectWebApi/Models/cTransformacionTipoMarcaDet.cs
+++ GeotimeConnectWebApi/Models/cTransformacionTipoMarcaDet.cs
@@ -1,8 +1,14 @@
-﻿namespace GeoTimeConnectWebApi.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace GeoTimeConnectWebApi.Models
 {
     public class cTransformacionTipoMarcaDet
     {
+        [Key]
         public int TRANSFORMACIONID { get; set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IDREGISTRO { get; set; }
         public bool VERIFICA_MARCA { get; set; }
         public string? HORA_INICIO { get; set; }
